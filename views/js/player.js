8fd44cd6989e85a5023fdbcf4b077bc49afae113
LanceCong <809556311@qq.com>
2016-10-20 14:01:08 +0800

    一些素材

--- views/js/player.js
+++ views/js/player.js
@@ -1,3 +1,19 @@
 /**
  * Created by admin on 2016/10/20.
  */
+$('input[type="range"]').rangeslider({
+    polyfill: false,
+
+    // Callback function
+    onInit: function() {},
+
+    // Callback function
+    onSlide: function(position, value) {
+        // console.log(position,value)
+    },
+
+    // Callback function
+    onSlideEnd: function(position, value) {
+        console.log(position,value)
+    }
+});
