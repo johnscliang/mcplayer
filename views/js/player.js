70d70fddb04c1a8814edacbb73c56af43f757f5b
LanceCong <809556311@qq.com>
2016-10-21 15:14:29 +0800

    基本完成播放器UI界面

--- views/js/player.js
+++ views/js/player.js
@@ -1,6 +1,7 @@
 /**
  * Created by admin on 2016/10/20.
  */
+//音量
 $('#range_time').rangeslider({
     polyfill:false
     ,onSlideEnd: function(position, value) {
@@ -13,4 +14,21 @@ $('#range_volume').rangeslider({
     ,onSlideEnd: function(position, value) {
         console.log(position,value)
     }
+});
+
+
+//音乐进程
+$("#music_progress").roundSlider({
+    radius: 70
+    ,width: 8
+    ,handleSize: "+16"
+    ,handleShape: "dot"
+    ,sliderType: "min-range"
+    ,min : 0
+    ,max : 200
+    ,showTooltip : false
+});
+
+$("#music_progress").on("change", function (e) {
+    console.log(e.value);
 });
